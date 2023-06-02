22280de0f2ec0832d756eb481a9a9b47f6170728
Hossein <hoseinmobasher@gmail.com>
2023-06-02 21:38:00 +0330

    Boostrap spring application

---
+++ src/main/java/com/bol/mancala/MancalaApp.java
@@ -0,0 +1,11 @@
+package com.bol.mancala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MancalaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MancalaApp.class, args);
+    }
+}
